d483daeaa6d0d43a2fe8dbeabe715d05b274d97f
SantiagoNavarroLlaguno <131417074+SantiagoNavarroLlaguno@users.noreply.github.com>
2023-09-27 09:50:39 -0600

    Delete 02_importdata.sql

--- 02_importdata.sql
+++
@@ -1,4 +0,0 @@
-\COPY Restaurants(id, rating, name, site, email, phone, street, city, state, lat, lng) FROM 'C:\Users\SNL\projects\melp_app\data\restaurantes.csv' WITH CSV HEADER;
-
--- NOTE: Ensure the path above points to the location of your CSV file. Adjust if necessary.
--- NOTE: After running this script, run the '03_optimization.sql' script to optimize the database for spatial queries.
